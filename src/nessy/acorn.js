66aa3aceabd93fe2e93a2dee34a605ffb2604171
mopozoff <morozovmav@gmail.com>
2015-06-30 14:16:42 +0300

    Added basis for modules

--- src/nessy/acorn.js
+++ src/nessy/acorn.js
@@ -1,3 +1,61 @@
+/*var nessy = {
+	run: function(options) {
+		options.init()
+		var update = function(elapsedTotal) {
+			window.requestAnimationFrame(update)
+			options.update(elapsedTotal)
+		}
+
+		window.requestAnimationFrame(update)
+	}
+}
+
+// caching elapsedTotal
+nessy.run({
+	init: function() {
+		nessy.timer = new nessy.Timer()
+	},
+	update: function(elapsedTotal) { 
+		nessy.timer.update(elapsedTotal)
+	}
+})
+
+// graphics
+nessy.run({
+	init: function() {
+		nessy.graphics = new nessy.Graphics()
+		nessy.draw = function() {}
+	},
+	update: function() {
+		nessy.graphics.clear()
+		nessy.draw()
+	}
+})
+
+// entities
+nessy.run({
+	init: function() {
+		nessy.entities = new nessy.EntityStore()
+		nessy.systems = new Array()
+		nessy.add = function(components, callback) {
+			nessy.systems.push({ components: components, callback: callback })
+		}
+	},
+	update: function() {
+		for (var entity of nessy.entities) {
+			for (var system of nessy.systems) {
+				var suitable = true
+				for (var component of system.components) {
+					suitable = entity[component] != null
+				}
+				if (suitable) {
+					system.callback(entity)
+				}
+			}
+		}
+	}
+})*/
+
 var nessy = {
 	run: function() {
 		nessy.timer = new nessy.Timer()
