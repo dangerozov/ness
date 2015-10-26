200ca2fa511cb616d3bb0d2fb7087b7093833603
Andrew Morozov <morozovmav@gmail.com>
2015-10-26 21:56:55 +0300

    Added func.overload

--- src/core/2func.js
+++ src/core/2func.js
@@ -1,13 +1,15 @@
 nessy.func = (() => {
 	var f = {};
-
+	
 	f.before = (func, pre) => (...args) => {
 		pre(...args);
 		return func(...args);
 	};
-	f.spread = (func) => (args) => func(...args);
-	f.map = (func, map) => (...args) => func(map(...args));
+	f.map = (func, map) => (...args) => map(func, ...args);
 	f.isUndefined = (obj) => typeof obj === "undefined";
+	
+	f.overload = (func, condition, map) => (...args) => 
+		condition(...args) ? map(func, ...args) : func(...args);
 	
 	return f;
 })();
@@ -18,6 +20,7 @@ nessy.func = ((func) => {
 		.chain("before", func.before)
 		.chain("spread", func.spread)
 		.chain("map", func.map)
+		.chain("overload", func.overload)
 		.value;
 		
 		/*		
@@ -34,11 +37,24 @@ nessy.func = ((func) => {
 
 })(nessy.func);
 
+
+// mapping args
 var fillRect = (x, y, width, height) => console.log(x, y, width, height);
 
-fillRect = nessy.func(fillRect) // (x,y,w,h) -> x,y,w,h
-	.spread()					// [x,y,w,h] -> (x,y,w,h) -> x,y,w,h
-	.map(rect => nessy.obj.values(rect, ["x", "y", "width", "height"])) // {x,y,w,h} -> [x,y,w,h] -> (x,y,w,h) -> x,y,w,h
+fillRect = nessy.func(fillRect) 
+	.map((func, rect) => func(rect.x, rect.y, rect.width, rect.height)) // {x,y,w,h} -> x,y,w,h
 	.value;
 
 fillRect({ x: 10, y: 20, width: 30, height: 40 });
+
+// overloading
+var abc = (x, y, z) => console.log(x, y, z);
+
+abc = nessy.func(abc)
+	.overload((...args) => args.length === 1, (func, obj) => func(obj.x, obj.y, obj.z))
+	.overload((...args) => args.length === 2, (func, x, y) => func(x, y, 3))
+	.value;
+
+abc({ x: 1, y: 2, z: 3 });
+abc(1, 2, 3);
+abc(1, 2);
