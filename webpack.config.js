7b362e1ad079b6fd9af56a42fe4889113e3157cc
Andrew Morozov <morozovmav@gmail.com>
2015-11-26 22:25:55 +0000

    Added source maps and watch script

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+var webpack = require('webpack');
+
 module.exports = {
 	entry: './src/main.ts',
 	output: {
 		filename: 'build/ness.js'
 	},
+	devtool: 'source-map',
 	resolve: {
 		extensions: ['', 'webpack.js', '.web.js', '.ts', '.js']
 	},
