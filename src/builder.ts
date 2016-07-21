44eed9f8473ae78fb8dd581da658229857405265
Andrew Morozov <morozovmav@gmail.com>
2016-07-21 22:11:28 +0300

    Refactored builder to accept curried functions

--- src/builder.ts
+++ src/builder.ts
@@ -1,50 +1,45 @@
-type Builder<TObject, TContext> = {
-	chain: (name: string, func: (context: TContext, ...args: any[]) => TContext) => Builder<TObject, TContext>;
-	cascade: (name: string, func: (context: TContext, ...args: any[]) => void) => Builder<TObject, TContext>;
-	unbox: (name: string, func: (context: TContext, ...args: any[]) => any) => Builder<TObject, TContext>;
-	value: (context: TContext) => TObject
-}
+interface Builder<TClass, TContext> {
+	value: (context: TContext) => TClass,
+	cascade: (name: string, func: (...args: any[]) => (context: TContext) => void) => Builder<TClass, TContext>,
+	chain: <TNewContext>(name: string, func: (...args: any[]) => (context: TContext) => TNewContext) => Builder<TClass, TNewContext>,
+	unbox: (name: string, func: (...args: any[]) => (context: TContext) => any) => Builder<TClass, TContext>
+};
 
-export let build = <TObject, TContext>() => {
-	
+export let build = <TClass, TContext>() => {
 	class Wrapper {
 		public value: TContext;
 		constructor(context: TContext) {
 			this.value = context;
 		}
 	}
-	
-	let addToPrototype = (name: string, func: (...args: any[]) => Wrapper | any) => {
-		(<{ [key: string]: any }>Wrapper.prototype)[name] = func;
-	};
-	
-	let builder: Builder<TObject, TContext>;
 
-	builder = {
-		value: (context) => <Wrapper & TObject>new Wrapper(context),
-		chain: (name, func) => {
-			addToPrototype(name, function(...args) {
-				(<Wrapper>this).value = func((<Wrapper>this).value, ...args);
-				return <Wrapper>this;
-			});
-			return builder;
+	const prototype: { [key: string]: any } = Wrapper.prototype;
+
+	let result: Builder<TClass, TContext> = {
+		value: (context: TContext) => <TClass & Wrapper>new Wrapper(context),
+		cascade: (name: string, func: (...args: any[]) => (context: TContext) => void) => {
+			prototype[name] = function(...args: any[]) {
+				func(...args)((<Wrapper>this).value);
+				return this;
+			}
+			return result;
 		},
-		cascade: (name, func) => {
-			addToPrototype(name, function(...args) {
-				func((<Wrapper>this).value, ...args);
-				return <Wrapper>this;
-			});
-			return builder;
+		chain: (name: string, func: (...args: any[]) => (context: TContext) => TContext) => {
+			prototype[name] = function(...args: any[]) {
+				(<Wrapper>this).value = func(...args)((<Wrapper>this).value);
+				return this;
+			}
+			return result;
 		},
-		unbox: (name, func) => {
-			addToPrototype(name, function(...args) {
-				return func((<Wrapper>this).value, ...args);
-			});
-			return builder;		
+		unbox: (name: string, func: <U>(...args: any[]) => (context: TContext) => U) => {
+			prototype[name] = function(...args: any[]) {
+				return func(...args)((<Wrapper>this).value);
+			}
+			return result;
 		}
 	};
-
-	return builder;
+	
+	return result;
 };
 
 type Logger = {
@@ -55,9 +50,9 @@ type Logger = {
 
 console.log("===== builder =====");
 var logger = build<Logger, (message: string) => void>()
-	.cascade("log", (log, text) => log(text))
-	.cascade("logError", (log, text) => log("Error: " + text))
-	.cascade("logInfo", (log, text) => log("Info: " + text))
+	.cascade("log", (text: string) => (log) => log(text))
+	.cascade("logError", (text: string) => (log) => log("Error: " + text))
+	.cascade("logInfo", (text: string) => (log) => log("Info: " + text))
 	.value;
 
 logger(console.log.bind(console))
