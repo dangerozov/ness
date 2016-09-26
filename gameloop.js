8cb21df429d4226ec1df0fae4074a026f5b3fe60
Andrew Morozov <morozovmav@gmail.com>
2016-09-26 19:38:31 +0300

    Merged event, task into eventSource

--- gameloop.js
+++ gameloop.js
@@ -1,4 +1,4 @@
-let noop = () => {};
+let noop = (_) => _;
 let array = {
     shift: (array) => {
         let previous = array[array.length - 1];
@@ -13,6 +13,61 @@ let array = {
         while(array.pop() != void 0);
     }
 };
+
+let eventSource = {
+    range: (start, count) => {
+        let result = eventSource.create();
+        setTimeout(() => result.send(1), 0);
+        return result;
+    },
+    create: () => {
+        let event = {
+            subs: [],
+            sub: function (cb) { return this.subs.push(cb); },
+            unsub: function (id) { this.subs.splice(id - 1, 1); },
+            subOnces: [[],[]],
+            subOnce: function(cb) { return this.subOnces[0].push(cb); },
+            send: function (ev) {
+                let call = cb => cb(ev);
+                if (this.subs === void 0) {
+                    console.log(this);
+                }
+                this.subs.forEach(call);
+
+                array.shift(this.subOnces); array.clear(this.subOnces[0]);
+                this.subOnces[1].forEach(call);
+            }
+        };
+
+        return event;
+    },
+    fromSubscribeOnce: (subscribeOnce) => {
+        let result = eventSource.create();
+        subscribeOnce(function on(ev) {
+            subscribeOnce(on);
+            result.send(ev);
+        });
+        return result;
+    },
+    map: (source, selector) => {
+        let result = eventSource.create();
+        source.sub(ev => result.send(selector(ev)));
+        return result;
+    },
+    bind: (source, selector) => {
+        let result = eventSource.create();
+        source.sub(ev => {
+            let evSrc = selector(ev);
+            evSrc.sub(ev => result.send(ev));
+        });
+        return result;
+    }
+};
+
+let cstm = eventSource.create();
+let cstm2 = eventSource.bind(cstm, ev => eventSource.create());
+cstm2.sub(console.log);
+cstm.send('qwe');
 
 // SubscribeOnce<T> monad - event you can subscribe only once
 // this is also Async<T> monad, because it has the same interface, i.e. returns result via callback
@@ -66,59 +121,12 @@ let task = {
     }
 };
 
-// using custom event
-let onCustom;
-let requestCustom = task.fromEvent(callback => onCustom = callback);
-
-onCustom('custom 0'); // no subscribers, nobody will process event
-requestCustom(console.log);
-requestCustom(console.log);
-onCustom('custom 1'); // two subscribers above will handle event only once
-requestCustom(console.log);
-onCustom('custom 2'); // one subscriber above will handle event only once
-
-let event = {
-    fromSubscribeOnce: (subscribeOnce, callback) => {
-        subscribeOnce(function on(ev) {
-            subscribeOnce(on);
-            callback(ev);
-        })
-    },
-    map: (subscribe, selector, callback) => {
-        subscribe((ev) => callback(selector(ev)));
-    }
-};
-
-let createEventHandler = () => {
-    let subscribers = [];
-    let handler = (ev) => {
-        subscribers.forEach(callback => callback(ev));
-    };
-    handler.subscribe = (callback) => subscribers.push(callback);
-    handler.unsubscribe = (id) => subscribers.splice(id - 1, 1);
-
-    return handler; 
-};
-
-let onframe = noop;
-event.fromSubscribeOnce(requestAnimationFrame, (ev) => onframe(ev));
-let ontime = noop;
-event.map((callback) => onframe = callback, (ev) => ev, (ev) => ontime("ontime"));
-//onframe = console.log;
-//ontime = console.log;
-
-// RequestAnimationFrame is a built-in SubscribeOnce event
-// that returns Elapsed time from the beggining of page load
-// here we map/Select its result to return additional info
-// previous - Elapsed time of previous requestAnimationFrame
-// delta - time between current and previous frames
-// if you (1 sec / delta) you will get FPS (frames per second) 
-let raf = task.map(requestAnimationFrame, (() => {
+let raf = eventSource.map(eventSource.fromSubscribeOnce(requestAnimationFrame), (() => {
     let time = {
         previous: 0,
         current: 0,
         delta: 0
-    }
+    };
 
     return (elapsed) => {
         time.previous = time.current;
@@ -129,48 +137,26 @@ let raf = task.map(requestAnimationFrame, (() => {
     };
 })());
 
-let onTime;
-let requestTime = task.fromEvent(callback => onTime = callback);
-raf(function on(time) {
-    raf(on);
-    onTime(time.delta);
-});
+let time = eventSource.map(raf, ev => ev.delta);
 
-let delay = (ms) => {
-    
-    let completed;
-    let waitTask = task.fromOneTimeEvent(callback => completed = callback);
+let delay = (ms) => {    
+    let completedEventSource = eventSource.create();
 
-    let elapsed = 0;
-    requestTime(function on(delta) {
-        console.log('upd', delta);
+    let elapsed = 0;    
+    time.subOnce(function on(delta) {
+        //console.log('upd', delta);
         elapsed += delta;
         if (elapsed >= ms) {
-            setTimeout(onTime, 0, elapsed - ms);
+            setTimeout(() => time.send(elapsed - ms), 0, time);
             console.log('finished', elapsed - ms);
-            completed();
+            completedEventSource.send();
         } else {
-            requestTime(on);
+            time.subOnce(on);
         }
     });
     
-    return waitTask;
+    return completedEventSource;
 };
-
-// let delay = (delay) => {
-//     return (callback) => {
-//         let elapsed = 0;
-//         raf(function on(time) {
-//             elapsed += time.delta;
-//             if (elapsed >= delay) {
-//                 callback(elapsed - delay);
-//                 console.log('delay:finished', elapsed - delay);
-//             } else {
-//                 raf(on);
-//             }
-//         })
-//     };
-// };
 
 let repeat = (createTask) => {
 
@@ -183,30 +169,24 @@ let repeat = (createTask) => {
     return noop;
 };
 
-// Async<T> monad - something that returns result via callback
-let async = {
-    // implementation of Promise.then/Task.ContinueWith
-    bind: (subscribe, selector) => (callback) => {
-        subscribe(r => selector(r)(r => callback(r)));
-    }
-};
-
-/*let w2000 = delay(2000);
-w2000(() => console.log('2000'));
-w2000(() => console.log('2000'));
-w2000(() => console.log('2000'));*/
+let w2000 = delay(2000);
+w2000.sub(() => console.log('2000'));
+w2000.sub(() => console.log('2000'));
+w2000.sub(() => console.log('2000'));
 
 // wait for window to load
-let preload = task.fromOneTimeEvent(callback => window.onload = callback);
-preload(console.log);
+let preload = eventSource.create();
+window.onload = (ev) => preload.send(ev);
+preload.sub(console.log);
 
-async.bind(preload, () => preload)(console.log);
-async.bind(preload, () => preload)(console.log);
-async.bind(preload, () => preload)(console.log);
+eventSource.bind(preload, () => preload).sub(console.log);
+eventSource.bind(preload, () => preload).sub(console.log);
+eventSource.bind(preload, () => preload).sub(console.log);
 
 let token = {
     cancelled: false
 };
+
 /*let game = async.bind(async.bind(
     delay(2000), // wait 2000 ms
     () => raf), // wait 1 frame
